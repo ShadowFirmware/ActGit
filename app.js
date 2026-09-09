59d10acea7431dd3e0234cabf67c8b245b92382b
LLUVIAGL <20233tn142@utez.edu.mx>
2026-09-08 18:50:08 -0600

    Agrega comportamiento del boton del caos

---
+++ app.js
@@ -0,0 +1,7 @@
+function cambiarColor() {
+    if (document.body.style.backgroundColor === "rgb(189, 224, 254)") {
+        document.body.style.backgroundColor = "#ffc8dd";
+    } else {
+        document.body.style.backgroundColor = "#bde0fe";
+    }
+}
